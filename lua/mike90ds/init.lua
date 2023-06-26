0ce7916c16af034ffc28e79a77bd23ab7100e7b1
mikequinto90d <137759189+mikequinto90d@users.noreply.github.com>
2023-06-26 22:17:10 +0800

    init nvim config for windows

---
+++ lua/mike90ds/init.lua
@@ -0,0 +1,11 @@
+require("mike90ds.settings")
+require("mike90ds.remap")
+
+local has = vim.fn.has
+local is_linux = has("unix")
+local is_win = has("win32")
+-- https://www.reddit.com/r/neovim/comments/vr68yl/checking_for_wsl_in_initlua/
+local is_wsl = (function()
+    local output = vim.fn.systemlist("uname -r")
+    return not not string.find(output[1] or "", "WSL")
+end)()
