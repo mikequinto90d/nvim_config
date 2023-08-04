010ab61107ad6167c4fb480f1ca7ec7de1a0a3e3
mikequinto90d <137759189+mikequinto90d@users.noreply.github.com>
2023-08-04 14:27:33 +0800

    telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -39,13 +39,13 @@ telescope.setup({
     extensions = {
         extensions = {
             fzf = {
-              fuzzy = true,                    -- false will only do exact matching
-              override_generic_sorter = true,  -- override the generic sorter
-              override_file_sorter = true,     -- override the file sorter
-              case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                                               -- the default case_mode is "smart_case"
-        }
-      }
+                fuzzy = true, -- false will only do exact matching
+                override_generic_sorter = true, -- override the generic sorter
+                override_file_sorter = true, -- override the file sorter
+                case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+                -- the default case_mode is "smart_case"
+            },
+        },
         -- live_grep_args = {
         --     list = {
         --         search_dirs = { "~/Documents/Github-repos" },
@@ -56,7 +56,7 @@ telescope.setup({
         --         search_dirs = { "~/Documents/Github-repos", "~/Documents/notes", "~/Documents/reference-repos" },
         --     },
         -- },
-    }, 
+    },
 })
 
 -- telescope.load_extension("fzf")
